a8af433a1df8b251a50e8dddd012f02cf1f7e921
JRyanDespraz <jess.rd@proton.me>
2024-09-03 11:35:01 +0200

    RandomForest_imputation.R

--- imputation scripts/RandomForest_imputation.R
+++ imputation scripts/RandomForest_imputation.R
@@ -1,13 +1,30 @@
 ############Random Forest imputation###########
-#https://cran.r-project.org/web/packages/missForest/index.html
+#Step 1: Load the dataset containing missingness
+# Load the necessary library
+library(readr)
 
+# Load the dataset containing missingness with semicolon-separated columns and decimal points
+dataset_with_missingness <- read_delim("MAR_10percent_missingness.csv", delim = ";", locale = locale(decimal_mark = "."))
+
+# Set data types
+dataset_with_missingness[, 1:7] <- lapply(dataset_with_missingness[, 1:7], as.factor)
+dataset_with_missingness$AgeGroup <- factor(dataset_with_missingness$AgeGroup, ordered = TRUE, levels = c("1", "2", "3", "4", "5"))
+dataset_with_missingness[, 9:13] <- lapply(dataset_with_missingness[, 9:13], factor, ordered = TRUE, levels = c("0", "0.5", "1", "2", "3"))
+dataset_with_missingness[, 14:41] <- lapply(dataset_with_missingness[, 14:41], as.numeric)
+
+#Step 2: Load the imputation library
+# Load the missForest library
+# https://cran.r-project.org/web/packages/missForest/index.html
 library(missForest)
 
+# Set seed for reproducibility
 set.seed(99)
-RF_imputed_dataset <- missForest(as.matrix(original_dataset), maxiter=, ntree=, verbose=TRUE)
-#Ryan-Despraz and Wissler (2024) set ntree equal to the number of variables
-#Ryan-Despraz and Wissler (2024) set maxiter (maximum number of iterations) to 10, but it may stop sooner if the stopping criterion is met
 
-#Create a dataframe for analyses
-final_rf_dataframe <- as.data.frame(RF_imputed_dataset$ximp)
+# Ensure dataset_with_missingness is a data frame
+dataset_with_missingness <- as.data.frame(dataset_with_missingness)
 
+# Perform random forest imputation
+imputed_rf_dataset <- missForest(dataset_with_missingness, maxiter = 10, ntree = 41, verbose=TRUE)
+
+# Extract the imputed data
+final_rf_imputed_dataset <- imputed_dataset$ximp
